323b86f2eac0daacb3ac8a82442c059228a54c9f
Dustin Van Buskirk <dev@vanbuskirk.me>
2019-03-21 13:16:34 -0700

    changes to allure in helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,6 @@ require 'chefspec'
 require 'chefspec/berkshelf'
 require 'allure-rspec'
 
-AllureRSpec.configure do |c|
-    c.output_dir = "/codefresh/volume/allure-results"
+RSpec.configure do |c|
+    c.include AllureRSpec::Adaptor
 end
