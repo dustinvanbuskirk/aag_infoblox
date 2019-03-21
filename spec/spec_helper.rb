787b7aab28f84972dfc91eeed9776f0079722e2e
Dustin Van Buskirk <dev@vanbuskirk.me>
2019-03-21 12:33:30 -0700

    adding in allure exporting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,8 @@
 # frozen_string_literal: true
 require 'chefspec'
 require 'chefspec/berkshelf'
+require 'allure-ruby-adaptor-api'
+
+AllureRubyAdaptorApi.configure do |c|
+    c.output_dir = "/codefresh/volume/reports"
+end
